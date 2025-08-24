5ecaf7fdf69841aea03b78218ed9042d95bea26d
Your Name <your-email@example.com>
2025-08-24 19:36:54 +0300

    add debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine , sessionmaker 
 
 from sqlalchemy_sandbox import Student
 
@@ -8,3 +8,17 @@ engine = create_engine('sqlite:///students.db')
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
+
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# create
+new_student = Student(name="Alice")
+session.add(new_student)
+session.commit()
+
+# read
+students = session.query(Student).all()
